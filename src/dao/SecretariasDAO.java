73afc4437702a78b0c0dd878f4d480f62a64224b
Morales2101 <morales2101@hotmail.com>
2016-10-15 01:21:17 -0300

    A primeira subida no GitHub

---
+++ src/dao/SecretariasDAO.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import model.Secretarias;
+
+
+@Repository
+public class SecretariasDAO {
+	private Connection conn;
+
+	@Autowired
+	public SecretariasDAO(DataSource dataSource) throws IOException {
+		try {
+			this.conn = dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new IOException(e);
+		}
+	}
+
+	public Secretarias secretarias(Secretarias secretarias) throws IOException {
+		String sqlInsert = "insert into Secretarias (IdSecret, Ativa, NomeSecret) " + "values(?,?,?)";
+		try (PreparedStatement pst = conn.prepareStatement(sqlInsert);) {
+			pst.setInt(1, secretarias.getId());
+			pst.setString(2, secretarias.getNome());
+
+			pst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IOException(e);
+		}
+		return secretarias;
+	}
+
+	public boolean existeSecretarias(Secretarias secretarias) {
+
+		if (secretarias == null) {
+			throw new IllegalArgumentException("Secretaria nao deve ser nulo");
+		}
+
+		try {
+			PreparedStatement stmt = this.conn
+					.prepareStatement("select * from Secretarias where IdSecret = ? and NomeSecret = ?");
+			stmt.setInt(1, secretarias.getId());
+			stmt.setString(2, secretarias.getNome());
+			ResultSet rs = stmt.executeQuery();
+
+			boolean encontrado = rs.next();
+			rs.close();
+			stmt.close();
+
+			return encontrado;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/service/ReclamacoesService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.ArrayList;
+
+import model.Reclamacoes;
+
+public class ReclamacoesService {
+
+	public ArrayList<Reclamacoes> listarReclamacoes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void criar(model.Reclamacoes reclamacoes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
